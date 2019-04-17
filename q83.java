b60438efdae1b135913ccccb98f061283cc8fac0
kefan43 <k27dong@edu.uwaterloo.ca>
2019-04-17 11:09:25 -0400

    + answer

--- q83.java
+++ q83.java
@@ -41,4 +41,15 @@ public class q83 {
         
         return trail.next;
     }
+    
+    public ListNode deleteDuplicates2(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        head.next = deleteDuplicates(head.next);
+        if (head.val == head.next.val) {
+            head = head.next;
+        }
+        return head;
+    }
 }
